87a024dcb85800a636dc26205ebb610a7114bc21
emuk213 <emilija.smile01@gmail.com>
2024-10-08 21:05:34 +0300

    5 kartus kreipiamasi i failu generavimo funkcija

--- Project_v0.2/Project_v0.2.cpp
+++ Project_v0.2/Project_v0.2.cpp
@@ -1,113 +1,23 @@
 #include <iostream>
-#include <limits>
 #include "MyLib2.h"
 #include "Stud2.h"
 
 
 int main()
 {
-    vector <Stud> vec1;
-    Stud temp;
-    int a, n, b;
-    string failoVardas;
-    cout << "Do you want Final score with vid or med (0 - vid, 1 - med)?" << endl;
-    try {
-        cin >> a;
-        if (cin.fail() || (a != 0 && a != 1)) {
-            throw runtime_error("Error: wrong input");
-        }
+    int a;
+    cout << "Ar norite generuoti naujus studentu sarasu failus? (0-ne, 1-taip)" << endl;
+    cin >> a;
+    if (a == 1) {
+        int b = 1000;
 
-        cout << "Do you want to read data from txt file (0 - no, 1 - yes)?" << endl;
+        for (int i = 0; i < 5; i++) {
 
-        cin >> b;
-        if (cin.fail() || (b != 0 && b != 1)) {
-            throw runtime_error("Error: wrong input");
-        }
-
-
-
-        if (b == 1) {
-            cout << "Input file name" << endl;
-            cin >> failoVardas;
-
-            readStudTxt(failoVardas, vec1);
-
-            if (a == 0) {
-                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid.)" << endl;
-
-                sort(vec1.begin(), vec1.end(), [](const Stud& a, const Stud& b) {
-                    return a.pavarde < b.pavarde;
-                    });
-
-                for (Stud& student : vec1) {
-                    skaiciuotiGalutiniBala(student);
-                    outputVid(student);
-
-                }
-            }
-            else if (a == 1) {
-                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (med.)" << endl;
-
-                sort(vec1.begin(), vec1.end(), [](const Stud& a, const Stud& b) {
-                    return a.pavarde < b.pavarde;
-                    });
-
-
-                for (Stud& student : vec1) {
-                    skaiciuotiGalutiniBala(student);
-                    outputMed(student);
-                }
-            }
-
-            /* Atspausdina duomenis is txt file
-               outputTxt(vec1);
-            */
-        }
-
-
-
-        else if (b == 0) {
-            cout << "How many students do you have?" << endl;
-
-            cin >> n;
-            if (cin.fail()) {
-                throw runtime_error("Error: input must be a number");
-            }
-
-            for (int i = 0; i < n; i++) {
-                cout << "Please input student data:" << endl;
-                input(temp);
-                skaiciuotiGalutiniBala(temp);
-                vec1.push_back(temp);
-                clean(temp);
-            }
-            if (a == 0) {
-                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (vid.)" << endl;
-
-                sort(vec1.begin(), vec1.end(), [](const Stud& a, const Stud& b) {
-                    return a.pavarde < b.pavarde;
-                    });
-                for (int i = 0; i < n; i++)
-                    outputVid(vec1.at(i));
-            }
-            else if (a == 1) {
-                cout << setw(15) << left << "Surname" << setw(10) << left << "Name" << setw(5) << right << "Final average score (med.)" << endl;
-
-                sort(vec1.begin(), vec1.end(), [](const Stud& a, const Stud& b) {
-                    return a.pavarde < b.pavarde;
-                    });
-                for (int i = 0; i < n; i++)
-                    outputMed(vec1.at(i));
-            }
-
+           kurti_faila("studentai" + to_string(b) + ".txt", b);
+           b *= 10;
         }
     }
-    catch (exception& e) {
-        cerr << e.what() << endl;
-        return 1;
-    }
-
     system("pause");
-
+    
     return 0;
 }
