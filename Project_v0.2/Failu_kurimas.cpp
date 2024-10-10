0ccf7583798b6bb80cab1f14e929ba6dd0a6dbd0
emuk213 <emilija.smile01@gmail.com>
2024-10-10 20:05:56 +0300

    Duomenu isvedimo i failus funkcija

--- Project_v0.2/Failu_kurimas.cpp
+++ Project_v0.2/Failu_kurimas.cpp
@@ -3,7 +3,7 @@
 #include "Stud2.h"
 
 void kurti_faila(const string& failas, int eil) {
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start0 = std::chrono::high_resolution_clock::now();
     ofstream studentai(failas);
     
     srand(time(0));
@@ -25,8 +25,19 @@ void kurti_faila(const string& failas, int eil) {
 
     studentai.close();
     cout << "Failas " << failas << " uzdarytas" << endl;
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    cout << "Failo generavimas uztruko: " << duration.count() << " sekundziu." << endl;
+    auto end0 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration0 = end0 - start0;
+    cout << "Failo generavimas uztruko: " << duration0.count() << endl;
    // system("pause");
+}
+
+void isvedimas(vector <Stud> vec, const string& failoPav) {
+    ofstream failas(failoPav);
+    failas << setw(15) << left << "Vardas" << setw(15) << left << "Pavarde" << setw(5) << right << "Galutinis balas vid." << endl;
+
+    for (const Stud& studentas : vec) {
+        failas << setw(15) << left << studentas.vardas << setw(15) << left << studentas.pavarde << setw(5) << setprecision(2) << fixed << right << studentas.galutinisVid << endl;
+    }
+
+    failas.close();
 }
