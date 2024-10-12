828858af136c278116f921fdad5fa15134f7950a
emuk213 <emilija.smile01@gmail.com>
2024-10-12 23:18:46 +0300

    naudoju timer klase

--- Project_v0.2/Project_v0.2.cpp
+++ Project_v0.2/Project_v0.2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MyLib2.h"
 #include "Stud2.h"
+#include "timer.h"
 
 
 int main()
@@ -19,48 +20,49 @@ int main()
            kurti_faila("studentai" + to_string(b) + ".txt", b);
            b *= 10;
         }
+      
     }
-    
-    int n = 1000;
-    for (int i = 0; i < 5; i++) {
-        vec1.reserve(n);
-        sigma.reserve(n);
-        beta.reserve(n);
-        auto start = std::chrono::high_resolution_clock::now();
-        readStudTxt("studentai" + to_string(n) + ".txt", vec1);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration = end - start;
-        cout << "\nFailo is " + to_string(n) + " irasu nuskaitymas uztruko : " << duration.count() << endl;
-        for (Stud& temp : vec1) {
-            skaiciuotiGalutiniBala(temp);
+        int n = 1000;
+        for (int i = 0; i < 5; i++) {
+            vec1.reserve(n);
+            sigma.reserve(n);
+            beta.reserve(n);
+            Timer t;
+            readStudTxt("studentai" + to_string(n) + ".txt", vec1);
+            double e = t.elapsed();
+            cout << "\nFailo is " + to_string(n) + " irasu nuskaitymas uztruko: " << e << " s\n";
+
+            for (Stud& temp : vec1) {
+                skaiciuotiGalutiniBala(temp);
+            }
+            Timer t1;
+            for (Stud& temp : vec1) {
+                kategorijos(sigma, beta, temp);
+            }
+            vec1.clear();
+
+            double e1 = t1.elapsed();
+            cout << to_string(n) + "irasu dalinimas i 2 grupes uztruko : " << e1 << "s\n";
+
+            Timer t2;
+            isvedimas(sigma, "sigma" + to_string(n) + ".txt");
+            double e2 = t2.elapsed();
+            cout << to_string(n) + "irasu sigma irasymo i faila uztruko: " << e2 << "s\n";
+
+            Timer t3;
+            isvedimas(beta, "beta" + to_string(n) + ".txt");
+            double e3 = t3.elapsed();
+            cout << to_string(n) + "irasu beta irasymo i faila uztruko: " << e3 << "s\n";
+
+            double totalTime = e + e1 + e2 + e3;
+            cout << "\n" + to_string(n) + "irasu testo laikas: " << totalTime << "s\n";
+
+            n *= 10;
         }
-        auto start1 = std::chrono::high_resolution_clock::now();
-        for (Stud& temp : vec1) {
-            kategorijos(sigma, beta, temp);
-        }
-        vec1.clear();
-        auto end1 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration1 = end1 - start1;
-        cout << to_string(n) + "irasu dalinimas i 2 grupes uztruko : " << duration1.count() << endl;
-
-        auto start2 = std::chrono::high_resolution_clock::now();
-        isvedimas(sigma, "sigma" + to_string(n) + ".txt");
-        auto end2 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration2 = end2 - start2;
-        cout << to_string(n) + "irasu sigma irasymo i faila uztruko: " << duration2.count() << endl;
-
-        auto start3 = std::chrono::high_resolution_clock::now();
-        isvedimas(beta, "beta" + to_string(n) + ".txt");
-        auto end3 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration3 = end3 - start3;
-        cout << to_string(n) + "irasu beta irasymo i faila uztruko: " << duration3.count() << endl;
-        std::chrono::duration<double> totalTime = duration + duration1 + duration2 + duration3;
-        cout << "\n" + to_string(n) + "irasu testo laikas: " << totalTime.count() << endl;
-
-        n *= 10;
-    }
 
     system("pause");
     
+   
+
     return 0;
 }
