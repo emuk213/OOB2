ceaff1f4b6b4fd4c0957730aa52e223d05f78b7b
emuk213 <emilija.smile01@gmail.com>
2024-10-12 22:10:11 +0300

    naujas atsitiktiniu skaiciu kurimas

--- Project_v0.2/Failu_kurimas.cpp
+++ Project_v0.2/Failu_kurimas.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MyLib2.h"
 #include "Stud2.h"
+#include "RandInt.h"
 
 void kurti_faila(const string& failas, int eil) {
     auto start0 = std::chrono::high_resolution_clock::now();
@@ -9,18 +10,20 @@ void kurti_faila(const string& failas, int eil) {
     srand(time(0));
  
     studentai << setw(15) << left << "Vardas" << setw(15) << left << "Pavarde";
-    for (int i = 1; i <= 9; i++) studentai << setw(5) << right << "ND" + to_string(i);
-    studentai << setw(5) << right << "Egz." << endl;
+    for (int i = 1; i <= 9; i++) studentai << setw(10) << right << "ND" + to_string(i);
+    studentai << setw(10) << right << "Egz." << endl;
 
+    constexpr int max = 10;
+    RandInt rnd{ 1, max };
     for (int i = 1; i <= eil; i++) {
         vector <int> nd(9);
         for (int j = 0; j < 9; j++) {
-            nd[j] = 1 + (rand() % 10);
+             nd[j] = rnd();
         }
 
         studentai << setw(15) << left << ("Vardas" + to_string(i)) << setw(15) << left << ("Pavarde" + to_string(i));
-        for (const int& paz : nd) studentai << setw(5) << right << paz;
-        studentai << setw(5) << right << (1 + (rand() % 10)) << endl;
+        for (const int& paz : nd) studentai << setw(10) << right << paz;
+        studentai << setw(10) << right << (rnd()) << endl;
     }
 
     studentai.close();
@@ -28,7 +31,6 @@ void kurti_faila(const string& failas, int eil) {
     auto end0 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration0 = end0 - start0;
     cout << "Failo generavimas uztruko: " << duration0.count() << endl;
-   // system("pause");
 }
 
 void isvedimas(vector <Stud> vec, const string& failoPav) {
