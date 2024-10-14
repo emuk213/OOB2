3143ab87449115bbe4235807e37f6b7751434cf6
emuk213 <emilija.smile01@gmail.com>
2024-10-14 22:44:13 +0300

    Update Timer.h

--- Project_v0.2/Timer.h
+++ Project_v0.2/Timer.h
@@ -12,18 +12,3 @@ public:
         return std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
     }
 };
-
-/*
-class Timer {
-public:
-    Timer() : start_time_point(std::chrono::high_resolution_clock::now()) { }
-
-    // Function to calculate elapsed time without resetting the start time
-    double elapsed() const {
-        auto end_time_point = std::chrono::high_resolution_clock::now();
-        return std::chrono::duration<double>(end_time_point - start_time_point).count();
-    }
-
-private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_time_point;
-};*/
