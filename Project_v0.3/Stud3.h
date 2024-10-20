3d0586822d6463d8215e20679edffbdcd1a16491
emuk213 <emilija.smile01@gmail.com>
2024-10-20 17:22:42 +0300

    pakeisti funkciju aprasymai

--- Project_v0.3/Stud3.h
+++ Project_v0.3/Stud3.h
@@ -10,20 +10,20 @@ struct Stud {
 };
 
 void input(Stud& Lok);
-void readStudTxt(const string& failoVardas, vector<Stud>& studentai);
+void readStudTxt(const string& failoVardas, list<Stud>& studentai);
 double skaiciuotiNdVid(const vector <int>& nd);
 double skaiciuotiNdMed(vector <int>& nd);
 void skaiciuotiGalutiniBala(Stud& Lok);
-void outputVid(Stud Lok);
-void outputMed(Stud Lok);
+void output(const list <Stud>& list1, int a);
 void clean(Stud& Lok);
 void kurti_faila(const string& failas, int eil);
-void isvedimas(vector <Stud>& vec, const string& failoPav);
-void kategorijos(vector<Stud>& sigma, vector<Stud>& beta, Stud& Lok);
+void isvedimas(list <Stud>& vec, const string& failoPav);
+void kategorijos(list<Stud>& list1, list<Stud>& sigma, list<Stud>& beta);
 bool lygintiVardas(Stud& a, Stud& b);
 bool lygintiPavarde(Stud& a, Stud& b);
 bool lygintiGalutinis(Stud& a, Stud& b);
-void sortByChoice(vector<Stud>& vec, int b);
+void sortByChoice(list<Stud>& vec, int b);
+
 
 #endif // STUD_H_INCLUDED
 
