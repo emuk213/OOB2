bbe76296729f5ebf2c6b3512e2496667d2d43461
emuk213 <emilija.smile01@gmail.com>
2024-10-20 12:57:00 +0300

    Add files via upload

---
+++ Project_v0.3/Timer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "MyLib3.h"
+class Timer {
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+public:
+    Timer() : start{ std::chrono::high_resolution_clock::now() } {}
+    void reset() {
+        start = std::chrono::high_resolution_clock::now();
+    }
+    double elapsed() const {
+        return std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
+    }
+};
