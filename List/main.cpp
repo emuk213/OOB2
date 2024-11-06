32b9871b0035ef0e899bb038c902909c0c9dafe3
emuk213 <emilija.smile01@gmail.com>
2024-11-06 20:04:01 +0200

    main su 2 strategija

--- List/main.cpp
+++ List/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     list <Stud> list1, sigma, beta;
     Stud temp;
-    int a, b, c, f, g, h, x;
+    int a, b, c, f, g, h, x, strategija;
     string failoVardas;
     cout << "Ar norite atlikti testus arba generuoti naujus failus (0-ne, 1-taip)?" << endl;
     cin >> h;
@@ -83,12 +83,12 @@ int main()
 
         cout << "Pagal ka norite rusiuoti duomenis? (0-varda, 1-pavarde, 2-galutini bala)" << endl;
         cin >> f;
+        cout << "Kokia strategija norite naudoti ( 1, 2)?" << endl;
+        cin >> strategija;
 
         int n = 1000;
         for (int i = 0; i < 5; i++) {
-            //vec1.reserve(n);
-            //sigma.reserve(n);
-            //beta.reserve(n);
+          
             Timer t;
             readStudTxt("studentai" + to_string(n) + ".txt", list1);
             double e = t.elapsed();
@@ -103,25 +103,47 @@ int main()
             double e1 = t1.elapsed();
             cout << to_string(n) + " irasu rusiavimas uztruko: " << e1 << " s\n";
 
-            Timer t2;
+           if (strategija == 1) {
+                Timer t2;
 
-            kategorijos(list1, sigma, beta);
-            //list1.clear();
-            double e2 = t2.elapsed();
-            cout << to_string(n) + " irasu dalinimas i 2 grupes uztruko: " << e2 << " s\n";
+                kategorijos(list1, sigma, beta);
+                double e2 = t2.elapsed();
+                cout << to_string(n) + " irasu dalinimas i 2 grupes uztruko: " << e2 << " s\n";
 
-            Timer t3;
-            isvedimas(sigma, "sigma" + to_string(n) + ".txt");
-            double e3 = t3.elapsed();
-            cout << to_string(n) + " irasu sigma irasymo i faila uztruko: " << e3 << " s\n";
+                Timer t3;
+                isvedimas(sigma, "sigma" + to_string(n) + ".txt");
+                double e3 = t3.elapsed();
+                cout << to_string(n) + " irasu sigma irasymo i faila uztruko: " << e3 << " s\n";
 
-            Timer t4;
-            isvedimas(beta, "beta" + to_string(n) + ".txt");
-            double e4 = t4.elapsed();
-            cout << to_string(n) + " irasu beta irasymo i faila uztruko: " << e4 << " s\n";
+                Timer t4;
+                isvedimas(beta, "beta" + to_string(n) + ".txt");
+                double e4 = t4.elapsed();
+                cout << to_string(n) + " irasu beta irasymo i faila uztruko: " << e4 << " s\n";
 
-            double totalTime = e + e1 + e2 + e3 + e4;
-            cout << "\n" + to_string(n) + " irasu testo laikas: " << totalTime << " s\n";
+                double totalTime = e + e1 + e2 + e3 + e4;
+                cout << "\n" + to_string(n) + " irasu testo laikas: " << totalTime << " s\n";
+            }
+
+           else if (strategija == 2) {
+                Timer t2;
+
+                kategorijos2(list1, beta);
+                double e2 = t2.elapsed();
+                cout << to_string(n) + " irasu dalinimas i 2 st2 grupes uztruko: " << e2 << " s\n";
+
+                Timer t3;
+                isvedimas(list1, "sigma" + to_string(n) + ".txt");
+                double e3 = t3.elapsed();
+                cout << to_string(n) + " irasu sigma irasymo i faila uztruko: " << e3 << " s\n";
+
+                Timer t4;
+                isvedimas(beta, "beta" + to_string(n) + ".txt");
+                double e4 = t4.elapsed();
+                cout << to_string(n) + " irasu beta irasymo i faila uztruko: " << e4 << " s\n";
+
+                double totalTime = e + e1 + e2 + e3 + e4;
+                cout << "\n" + to_string(n) + " irasu testo laikas: " << totalTime << " s\n";
+            }
 
             n *= 10;
         }
