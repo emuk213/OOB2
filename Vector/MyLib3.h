92d79281cd6cbf9ace0bfc1ea147986e0bc53ae3
emuk213 <emilija.smile01@gmail.com>
2024-11-06 19:29:59 +0200

    1strategija su vector

---
+++ Vector/MyLib3.h
@@ -0,0 +1,44 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <fstream>
+#include <sstream> //del getline
+#include <ios> //(del streamsize)
+#include <limits> //(del numeric limits)
+#include <cstdlib> //(del rand, exit())
+#include <stdexcept>
+#include <exception>
+#include <random>
+#include <ctime>
+#include <chrono>
+
+
+using std::endl;
+using std::cout;
+using std::cin;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::sort;
+using std::max;
+using std::ifstream;
+using std::streamsize;
+using std::numeric_limits;
+using std::string;
+using std::vector;
+using std::runtime_error;
+using std::istringstream;
+using std::cerr;
+using std::exception;
+using std::ofstream;
+using std::to_string;
+
+
+#endif // MYLIB_H_INCLUDED
