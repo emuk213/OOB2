75b7f58fe484d076251e97ae790f4d191d2b8045
emuk213 <emilija.smile01@gmail.com>
2024-11-07 18:46:51 +0200

    Add files via upload

---
+++ List/include/Stud3.h
@@ -0,0 +1,32 @@
+#ifndef STUD_H_INCLUDED
+#define STUD_H_INCLUDED
+#include "MyLib3.h"
+
+struct Stud {
+	string vardas, pavarde;
+	vector <int> nd;
+	int egz;
+	double galutinisVid, galutinisMed;
+};
+
+void input(Stud& Lok);
+void readStudTxt(const string& failoVardas, list<Stud>& studentai);
+double skaiciuotiNdVid(const vector <int>& nd);
+double skaiciuotiNdMed(vector <int>& nd);
+void skaiciuotiGalutiniBala(Stud& Lok);
+void output(const list <Stud>& list1, int a);
+void clean(Stud& Lok);
+void kurti_faila(const string& failas, int eil);
+void isvedimas(list <Stud>& vec, const string& failoPav);
+void kategorijos(list<Stud>& list1, list<Stud>& sigma, list<Stud>& beta);
+
+void kategorijos2(list<Stud>& list1, list<Stud>& beta);
+void kategorijos3(list<Stud>& list1, list<Stud>& beta);
+
+bool lygintiVardas(Stud& a, Stud& b);
+bool lygintiPavarde(Stud& a, Stud& b);
+bool lygintiGalutinis(Stud& a, Stud& b);
+void sortByChoice(list<Stud>& vec, int b);
+
+#endif // STUD_H_INCLUDED
+
