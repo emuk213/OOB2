5f40f2114a78acec4a8a34eb756852651023cb17
emuk213 <emilija.smile01@gmail.com>
2024-11-27 18:38:15 +0200

    Update Stud3.h

--- Vector/include/Stud3.h
+++ Vector/include/Stud3.h
@@ -14,10 +14,43 @@ public:
 	Stud(istream& is);
 	istream& readStudent(istream&);
 
-	inline string vardas() const { return vardas_; }
-	inline string pavarde() const { return pavarde_; }
+	/*
+	Stud(const Stud& op) : vardas_(op.vardas_), pavarde_(op.pavarde_), nd_(op.nd_), egz_(op.egz_),
+		galutinisVid_(op.galutinisVid_), galutinisMed_(op.galutinisMed_) {}*/
+	
+	Stud(const Stud& op);
+	
+	//Stud(string, string, int, double, double);
+	Stud(const Stud& c) {
+		vardas_ = c.vardas_;
+		pavarde_ = c.pavarde_;
+		nd_ = c.nd_;
+		egz_ = c.egz_;
+		galutinisVid_ = c.galutinisVid_;
+		galutinisMed_ = c.galutinisMed_;
+
+	}
+	Stud& operator=(const Stud& op) {
+		if (this != &op) {  // Apsauga nuo savees priskyrimo
+			vardas_ = op.vardas_;
+			pavarde_ = op.pavarde_;
+			nd_ = op.nd_;
+			egz_ = op.egz_;
+			galutinisVid_ = op.galutinisVid_;
+			galutinisMed_ = op.galutinisMed_;
+		}
+		return *this;
+	}
+
+	
+	Stud& operator=(const Stud& op);
+
+
+	inline string vardas() const { return vardas_; }    // get'eriai, inline
+	inline string pavarde() const { return pavarde_; }  // get'eriai, inline
 	inline double galutinisVid() const { return galutinisVid_; }
 	inline double galutinisMed() const { return galutinisMed_; }
+
 	void input();
 	void skaiciuotiGalutiniBala();
 	void clean();
@@ -26,17 +59,33 @@ public:
 	void setNd(const vector<int>& nd) {nd_ = nd;}
 	void setEgz(int egz) {egz_ = egz;}
 
-	~Stud() { }
-};
+	~Stud() = default;
 
-void readStudTxt(const string& failoVardas, vector<Stud>& studentai);
+	friend istream& operator>>(istream& in, Stud& student);
+	friend ostream& operator<<(ostream& out, const Stud& student);
+	void output(const vector<Stud>& vec, const string& failoPav);
+	void output(const vector<Stud>& vector1, int a);
+
+	void input(const string& failoVardas, vector<Stud>& studentai);
+	void input(const string& failas, int eil);
+};
+/*
+Stud::Stud(string vardas, string pavarde,  int egz, double vid, double med) {
+	vardas_ = vardas;
+	pavarde_ = pavarde;
+	//nd_ = nd;
+	egz_ = egz;
+	galutinisVid_ = vid;
+	galutinisMed_ = med;
+}*/
+
+//void readStudTxt(const string& failoVardas, vector<Stud>& studentai);
 
 static double skaiciuotiNdVid(const vector <int>& nd);
 static double skaiciuotiNdMed(vector <int>& nd);
 
-void output(const vector <Stud>& vector1, int a);
-void kurti_faila(const string& failas, int eil);
-void isvedimas(const vector<Stud>& vec, const string& failoPav);
+
+
 
 void kategorijos3(vector<Stud>& vector1, vector<Stud>& beta);
 
