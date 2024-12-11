b91f9c632c5ca4a9c58ea969118881fd1faa4f76
emuk213 <emilija.smile01@gmail.com>
2024-12-11 19:21:06 +0200

    pridedu testu faila

---
+++ Vector/Tests/test.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "../Stud3.h"
+#include "../Stud3.cpp"
+
+
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+TEST(StudTest, Constructor) {
+    Stud student;
+    EXPECT_EQ(student.getVardas(), "");
+    EXPECT_EQ(student.getPavarde(), "");
+    EXPECT_EQ(student.getEgz(), 0);
+    ///EXPECT_EQ(student.getNd(), vector<int>{});
+    EXPECT_TRUE(student.getNd().empty());
+
+}
+
+TEST(StudTest, OutputOp) {
+    Stud student("John", "Doe", { 10, 9, 8 }, 10);
+    student.skaiciuotiGalutiniBala();
+    std::ostringstream output;
+    output << student;
+    EXPECT_TRUE(output.str().find("Doe") != string::npos);
+    EXPECT_TRUE(output.str().find("John") != string::npos);
+    EXPECT_TRUE(output.str().find("9.6") != string::npos);
+}
+
+TEST(StudTest, Setters) {
+    Stud student;
+    student.setVardas("Algis");
+    student.setPavarde("Fol");
+    student.setEgz(5);
+    student.setNd({ 3, 4, 10, 8 });
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
